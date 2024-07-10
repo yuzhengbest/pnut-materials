cd2fc2438871e9dbe874968db46bb64e1cf07218
yuzheng <yuzhengbest@yeah.net>
2024-07-10 23:02:47 +0800

    [chore]: 初始化仓库

---
+++ packages/components/card/src/CardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul
+    class="pnut-card-list"
+    :style="cardListStyle"
+  >
+    <slot name="prepend" />
+    <slot />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  minWidth: {
+    type: Number,
+    default: 256
+  }
+})
+
+const cardListStyle = computed(() => {
+  return {
+    'grid-template-columns': `repeat(auto-fill, minmax(${parseInt(props.minWidth)}px, 1fr))`
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+.pnut-card-list {
+  display: grid;
+  grid-auto-rows: min-content;
+  grid-auto-flow: row dense;
+  gap: 16px;
+}
+</style>
---
+++ packages/components/empty/src/empty.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="pnut-empty">
+    <div :class="{
+      'has-title': hasTitle || title,
+      [`h-breakpoint--${size}`]: !!size
+    }">
+      <template v-if="hasDefault">
+        <slot></slot>
+      </template>
+      <template v-else>
+        
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType, computed, useSlots } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String as PropType<'sm' | 'md' | 'lg'>,
+    default: 'md'
+  },
+  theme: {
+    type: String as PropType<'dark' | 'light'>,
+    default: 'light'
+  },
+  img: {
+    type: String,
+    default: '404'
+  },
+  title: {
+    type: String,
+    default: null
+  },
+  description: {
+    type: String,
+    default: null
+  },
+  placement: {
+    type: String,
+    default: 'bottom'
+  }
+})
+
+const slots = useSlots()
+
+const hasDefault = computed(() => slots.default)
+
+const hasImg = computed(() => slots.img)
+
+const hasTitle = computed(() => slots.title)
+
+const hasDescription = computed(() => slots.description)
+
+const hasButtons = computed(() => slots.buttons)
+
+
+</script>
